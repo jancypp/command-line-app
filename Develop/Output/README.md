1a57e19b8ddbe0f8b0e9930874db6100a33859ea
jancypp <120283327+jancypp@users.noreply.github.com>
2023-02-19 18:05:23 -0700

    updated questions on index.js and update data pull on generat markdown file

---
+++ Develop/Output/README.md
@@ -0,0 +1,33 @@
+# Command Line App
+
+  ## Description
+  Command Line App
+  
+  ## Table of Contents (Optional)
+  
+  If your README is long, add a table of contents to make it easy for users to find what they need.
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  
+  ## Installation
+  npm i
+  
+  ## Usage
+  Generate a README
+  
+  ## Contributors
+  Jancy and Phil
+
+  
+  ## License
+Apache  
+  
+  ## Question
+ jancypp
+ jancypolzkill@hotmail.com
+
+  ## Tests
+  terminal commands
---
+++ Develop/index.js
@@ -0,0 +1,70 @@
+
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer")
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs')
+
+// TODO: Create an array of questions for user input
+const questions = [{
+    type: 'input',
+    message: 'What is the project title?',
+    name: 'projectTitle',
+},
+{
+    type: 'input',
+    message: 'What is the description?',
+    name: 'description',
+},
+{
+    type: 'input',
+    message: 'What are the installation instructions?',
+    name: 'installationInstructions',
+},
+{
+    type: 'input',
+    message: 'What are you using this application for?',
+    name: 'applicationUsage',
+},
+{
+    type: 'input',
+    message: 'Who contributed to the program?',
+    name: 'contributors',
+},
+{
+    type: 'input',
+    message: 'How will you test this application?',
+    name: 'testInstructions',
+},
+{
+    type: 'list',
+    message: 'What license do you want?',
+    name: 'licenseType',
+    choices: ['Apache', 'IBM', 'MIT', 'Mozilla', 'Unlicense']
+},
+{
+    type: 'input',
+    message: 'What is your github username?',
+    name: 'userName',
+},
+{
+    type: 'input',
+    message: 'What is your email?',
+    name: 'emailAddress',
+
+}];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { 
+    fs.writeFileSync(fileName, data)
+}
+
+// TODO: Create a function to initialize app
+function init() {
+    inquirer.prompt(questions)
+    .then(data => {
+writeToFile('./Output/README.md', generateMarkdown(data))
+    })
+ }
+
+// Function call to initialize app
+init();
